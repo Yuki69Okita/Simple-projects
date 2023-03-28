0445a0dfa492e2f634df291d776153de7a2b49c7
Valentina Marinova <61772169+Yuki69Okita@users.noreply.github.com>
2023-03-28 12:47:02 +0300

    Fixed, kind of

--- Guessing game/Version 2.py
+++ Guessing game/Version 2.py
@@ -4,16 +4,14 @@ import random
 def computer_guess_num():
     possible_num = list(range(1, 11))
 
-    max_lives = 4
-    min_lives = 1
+    max_lives = 3
+    min_lives = 0
 
     feedback = ""
     guess = 0
 
     print("Think of a number between 1 and 10.")
     while max_lives > min_lives:
-
-        max_lives -= 1
 
         if feedback == "c":
             print("Yey! I've guessed it!")
@@ -25,14 +23,12 @@ def computer_guess_num():
         elif feedback == "h":
             possible_num = [num for num in possible_num if num > guess]
 
-        elif feedback == "":
+        elif feedback == "" and max_lives == 3:
             pass
 
         else:
-            while feedback != "h" and feedback != "l" and feedback != "c":
-                print("Please enter 'h' for Higher, 'l' for lower or 'c' for Correct.")
-                print("-------------------------------------------------------------")
-                feedback = input("Is it 'h' - Higher, 'l' - lower or 'c' - Correct: ")
+            print("Please enter 'h' for Higher, 'l' for lower or 'c' for Correct!")
+            break
 
         if len(possible_num) == 0:
             print("Not possible!")
@@ -43,16 +39,23 @@ def computer_guess_num():
         print(f"Number: {guess}")
 
         if max_lives != 1:
-            feedback = input("Is it 'h' - Higher, 'l' - lower or 'c' - Correct: ")
-
-        print("-------------------------------------------------------------")
-
-    else:
-        if guess == 10 or guess == 1:
-            print("Yay! Somehow I did win.")
+            feedback = input("Is it 'h' - Higher, 'l' - lower or 'c' - Correct: ").lower()
 
         else:
+            feedback = input("Is it correct? 'y' - Yes and 'n' - No: ").lower()
+
+        print("-------------------------------------------------------------")
+        max_lives -= 1
+    else:
+
+        if feedback == "y" or guess == 10 and len(possible_num) == 1 or guess == 1 and len(possible_num) == 1:
+            print("Ha ha! I win, you lost!")
+
+        elif feedback == "n":
             print("Oh, no! I've lost!")
+
+        else:
+            print("Please enter 'y' for Yes and 'n' for No!")
 
 
 computer_guess_num()
